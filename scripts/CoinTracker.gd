8fbaea861e24b9f485169514413f4db1f471e0f0
pfitzpatrick <pfitzpatrick12@qub.ac.uk>
2021-01-31 04:33:35 +0000

    Adding phone functionality

--- scripts/CoinTracker.gd
+++ scripts/CoinTracker.gd
@@ -2,7 +2,7 @@ extends Node
 
 class_name CoinTracker
 
-var _fait_value: float
+var _fait_value: float setget , get_fait_value
 
 
 func increase_price(delta: float):
@@ -14,6 +14,9 @@ func decrease_price(delta: float):
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func get_fait_value():
+	return _fait_value
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- scripts/StoryManager.gd
+++ scripts/StoryManager.gd
@@ -8,13 +8,28 @@ var _stories = []
 var _current_story: Story setget ,get_current_story
 var _story_factory = StoryFactory.new()
 
+
+
 func evaluate_vote(vote) -> bool:
 	if _current_story.get_is_true():
 		return vote
 	else:
 		return !vote
 
+#Pick random story from array _stories which has not been chosen
 func get_current_story() -> Story:
+
+	#Ensure randomness
+	randomize()
+	var was_shown = true
+	
+	#Loops randomly until an unshown sample is chosen
+	while(was_shown == true):
+		_current_story = _stories[rand_range(0, (_stories.size()-1))]
+		was_shown = _current_story.get_was_shown()
+	
+	_mark_story_as_shown()
+	
 	return _current_story
 
 # Called when the node enters the scene tree for the first time.
